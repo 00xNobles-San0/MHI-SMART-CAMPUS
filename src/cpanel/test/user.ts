f78f92c1b77e95f2b558bbf04a96a21ed0d4171e
abdelrahman <https://github.com/abdo6999>
2023-11-26 16:13:46 +0200

    update

--- src/cpanel/test/user.ts
+++ src/cpanel/test/user.ts
@@ -1,23 +1,3 @@
 import UserService from "../services/users"
-import json from "./student.json"
 const service = new UserService()
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-for (let i = 0; i < json.length; i++) {
-  await delay(2000)
-  console.log(i)
-  const student = json[i];
-  service.createUser(
-    {
-      id:"",
-      userId:student.id,
-      name: student.name,
-      role: "student",
-      email: "",
-      phoneNumber: "",
-    }
-  )
-}
-
-
-// service.deleteAllUsers()
